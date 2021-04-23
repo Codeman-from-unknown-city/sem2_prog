149fedbd54ef7be92cb68c34ac118e38cef83e64
Sergei <i_truschev@mail.ru>
2021-04-23 20:09:41 +0300

    fix gramma mistakes

--- lab3/src/main.cpp
+++ lab3/src/main.cpp
@@ -19,13 +19,13 @@ int main() {
 		analyst.handle_stop(stop);
 	std::cout << "The street with max number of stops:" << std::endl;
 	std::cout << analyst.max_street.name << std::endl;;
-	std::cout << "The routes with max number of stops:" << std::endl;
+	std::cout << "Routes with max number of stops:" << std::endl;
 	for (auto& type_of_transport: types_of_vehicles) {
 		std::cout << type_of_transport << ": ";
 		std::cout << analyst.max_routes_by_stops[type_of_transport].name;
 		std::cout << std::endl;
 	}
-	std::cout << "The routes with max dists:" << std::endl;
+	std::cout << "Routes with max dists:" << std::endl;
 	for (auto& type_of_transport: types_of_vehicles) {
 		std::cout << type_of_transport << ": ";
 		std::cout << analyst.max_routes_by_dist[type_of_transport].name; 
@@ -33,3 +33,4 @@ int main() {
 	}
     return 0;
 }
+

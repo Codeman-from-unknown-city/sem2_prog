223eb0daebe2eadda4c3f24071df4475fdd3db29
Sergei <i_truschev@mail.ru>
2021-05-23 17:38:33 +0300

    fix template

--- lab5/RingBuffer.h
+++ lab5/RingBuffer.h
@@ -24,14 +24,14 @@ public:
 
     class Iterator {
     public:
-        Iterator(RingBuffer<T>* obj, int elem_ind);
+        Iterator(RingBuffer* obj, int elem_ind);
 
         Iterator(const Iterator& other);
 
         Iterator& operator=(const Iterator& other);
 
-        friend bool operator==(const RingBuffer<T>::Iterator& i1,
-                               const RingBuffer<T>::Iterator& i2);
+        friend bool operator==(const RingBuffer::Iterator& i1,
+                               const RingBuffer::Iterator& i2);
 
         Iterator& operator++();
 
